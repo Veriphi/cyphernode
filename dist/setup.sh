a2fee89a50a34445d6875db32f0e73aa63bd6331
Nathaniel <nat.kitzke@gmail.com>
2019-11-18 22:36:38 -0500

    change cypherapps repo point

--- dist/setup.sh
+++ dist/setup.sh
@@ -698,7 +698,7 @@ sanity_checks_pre_install() {
 
 install_apps() {
   if [ ! -d "$current_path/apps" ]; then
-    local apps_repo="https://github.com/SatoshiPortal/cypherapps.git"
+    local apps_repo="https://github.com/nk1tz/cypherapps.git"
     echo "   [32mclone[0m $apps_repo into apps"
     docker run --rm -v "$current_path":/git --entrypoint git cyphernode/cyphernodeconf:$CONF_VERSION clone --single-branch -b ${CYPHERAPPS_VERSION} "$apps_repo" /git/apps > /dev/null 2>&1
   fi
